ab09991e79b945c5dd4576bddf64ced095d7674c
Patrick <patrick.munyaneza@gmail.com>
2017-04-21 22:01:59 +0200

    add angular2 bese setup files

---
+++ app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        <div class="ui secondary segment"> 
+            <h1 class="ui header"> Angular2 successfully set up and running! </h1>
+        </div>
+    `
+})
+export class AppComponent{
+
+}
