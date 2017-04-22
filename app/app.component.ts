71365e3c3c6f4478bf3d510a1f81d103257b0c8d
Patrick <patrick.munyaneza@gmail.com>
2017-04-22 22:31:32 +0200

    change welcome message and style

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
     template: `
-        <div class="ui secondary segment"> 
-            <h1 class="ui header"> Angular2 successfully set up and running! </h1>
+        <div class="ui  page grid"> 
+            <div class="row">
+                <div class="column">
+                    <div class="ui massive message main">
+                        <h1 class="ui header"> The App Works! </h1>
+                        <p> 
+                            Angular2 environment up and running.
+                        </p>
+                    </div>
+                </div>   
+            </div>
         </div>
     `
 })
